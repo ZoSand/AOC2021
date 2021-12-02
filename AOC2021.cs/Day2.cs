2bad83a08f5ded02947f0acbf7d6b6edb3a22d15
ZoSand <valentin.lambert.ba@gmail.com>
2021-12-02 11:32:38 +0100

    Day2 => cs removed and sort Usings instructions

---
+++ AOC2021.cs/Day2.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AOC2021.cs
+{
+    internal class Day2
+    {
+        public static int Resolve(int _part, string _filename)
+        {
+            KeyValuePair<string, int>[] data;
+            try
+            {
+                data = File.ReadAllText(_filename)
+                    .Split('\n')
+                    .Select(s =>
+                    {
+                        int n;
+                        string[] parts = s.Split(' ');
+                        return new KeyValuePair<string, int>(
+                            parts[0], 
+                            int.TryParse(
+                                parts.Count() > 1 
+                                ? parts[1]
+                                : "0", out n) 
+                            ? n 
+                            : 0);
+                    })
+                    .ToArray();
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
+
+            switch (_part)
+            {
+                case 1:
+                    return Day2_1(data);
+                case 2:
+                    return Day2_2(data);
+                default:
+                    return -1;
+            }
+        }
+
+        static int Day2_1(KeyValuePair<string, int>[] _dayData)
+        {
+            int depth = 0;
+            int horizontalPosition = 0;
+            foreach (KeyValuePair<string, int> kvp in _dayData)
+            {
+                switch (kvp.Key)
+                {
+                    case "forward":
+                        horizontalPosition += kvp.Value;
+                        break;
+                    case "up":
+                        depth -= kvp.Value;
+                        break;
+                    case "down":
+                        depth += kvp.Value;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return depth * horizontalPosition;
+        }
+
+        static int Day2_2(KeyValuePair<string, int>[] _dayData)
+        {
+            int depth = 0;
+            int horizontalPosition = 0;
+            int aim = 0;
+            foreach (KeyValuePair<string, int> kvp in _dayData)
+            {
+                switch (kvp.Key)
+                {
+                    case "forward":
+                        horizontalPosition += kvp.Value;
+                        depth += aim * kvp.Value;
+                        break;
+                    case "up":
+                        aim -= kvp.Value;
+                        break;
+                    case "down":
+                        aim += kvp.Value;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return depth * horizontalPosition;
+        }
+    }
+}
--- AOC2021.cs/Program.cs
+++ AOC2021.cs/Program.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.IO;
-using System.Linq;
-using System.Text.Json;
 
 namespace AOC2021.cs
 {
@@ -13,6 +8,9 @@ namespace AOC2021.cs
         {
             Console.WriteLine("December 1st (1):" + Day1.Resolve(1, "./data/day1.txt"));
             Console.WriteLine("December 1st (2):" + Day1.Resolve(2, "./data/day1.txt"));
+
+            Console.WriteLine("December 2nd (1):" + Day2.Resolve(1, "./data/day2.txt"));
+            Console.WriteLine("December 2nd (2):" + Day2.Resolve(2, "./data/day2.txt"));
 
             Console.Read();
         }
