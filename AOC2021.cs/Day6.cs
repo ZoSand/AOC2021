6a1b365284601cc6ac0acc0be56cb0091f7085bd
ZoSand <valentin.lambert.ba@gmail.com>
2021-12-06 17:53:21 +0100

    Day 6 => cs; Days 4 and 5 throwing exceptions => cs

---
+++ AOC2021.cs/Day6.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AOC2021.cs
+{
+    class Day6
+    {
+        public static long Resolve(int _part, string _filename)
+        {
+            int[] data;
+            try
+            {
+                data = File.ReadAllText(_filename)
+                    .Split(',')
+                    .Select(s =>
+                    {
+                        int n;
+                        return int.TryParse(s, out n) ? n : 0;
+                    })
+                    .ToArray();
+            }
+            catch (Exception)
+            {
+
+                return -1;
+            }
+
+            switch (_part)
+            {
+                case 1:
+                    return Day6_1(data);
+                case 2:
+                    return Day6_2(data);
+                default:
+                    return -1;
+            }
+        }
+
+        static long Day6_1(int[] _dayData)
+        {
+            return NumberOfFishesAfter(80, _dayData);
+        }
+
+        static long Day6_2(int[] _dayData)
+        {
+            return NumberOfFishesAfter(256, _dayData);
+        }
+
+        static long NumberOfFishesAfter(long _numberOfDays, int[] _dayData)
+        {
+            const long maxage = 8;
+            long[] age = new long[maxage + 1];
+            foreach (long i in _dayData)
+            {
+                age[i]++;
+            }
+
+            for (long i = 0; i < _numberOfDays; i++)
+            {
+                long a0 = age[0];
+                for (long j = 1; j <= maxage; j++)
+                {
+                    age[j - 1] = age[j];
+                    age[j] = 0;
+                }
+                age[8] += a0;
+                age[6] += a0;
+            }
+
+            long e = 0;
+            for (long j = 0; j <= maxage; j++) e += age[j];
+            return e;
+        }
+    }
+}
--- AOC2021.cs/Program.cs
+++ AOC2021.cs/Program.cs
@@ -6,6 +6,7 @@ namespace AOC2021.cs
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("December 1st (1):" + Day1.Resolve(1, "./data/day1.txt"));
             Console.WriteLine("December 1st (2):" + Day1.Resolve(2, "./data/day1.txt"));
 
@@ -14,6 +15,17 @@ namespace AOC2021.cs
 
             Console.WriteLine("December 3rd (1):" + Day3.Resolve(1, "./data/day3.txt"));
             Console.WriteLine("December 3rd (2):" + Day3.Resolve(2, "./data/day3.txt"));
+
+            /*
+            Console.WriteLine("December 4th (1):" + Day4.Resolve(1, "./data/day4.txt"));
+            Console.WriteLine("December 4th (2):" + Day4.Resolve(2, "./data/day4.txt"));
+                                                       
+            Console.WriteLine("December 5th (1):" + Day5.Resolve(1, "./data/day5.txt"));
+            Console.WriteLine("December 5th (2):" + Day5.Resolve(2, "./data/day5.txt"));
+            */
+
+            Console.WriteLine("December 6th (1):" + Day6.Resolve(1, "./data/day6.txt"));
+            Console.WriteLine("December 6th (2):" + Day6.Resolve(2, "./data/day6.txt"));
 
             Console.Read();
         }
